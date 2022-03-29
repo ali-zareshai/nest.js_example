c0a3a48c88e287992f960bcae5a2a1225a8b5b67
ali_zareshaho <alititi96@gmail.com>
2022-03-29 13:30:38 +0430

    add jwt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,10 +2,13 @@ import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { LoginDate, User } from './dto';
 import * as argon from "argon2";
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { use } from 'passport';
 
 @Injectable()
 export class AuthService {
-    constructor(private prisma:PrismaService){}
+    constructor(private prisma:PrismaService,private jwt:JwtService,private config:ConfigService){}
 
     async addUser(user:User){
         const hashPassword =await argon.hash(user.password);
@@ -36,6 +39,23 @@ export class AuthService {
         }
 
         delete user.pass;
-        return user;
+        const token =await this.getToken(user.id,user.name,user.email);
+        return {
+            ...user,
+            token:token
+        };
+    }
+
+    async getToken(id:Number ,name:string,email:string){
+        const payload={
+            sub:id,
+            name:name,
+            email:email
+        };
+
+        return await this.jwt.signAsync(payload,{
+            expiresIn:"60m",
+            secret:this.config.get("JWT_SECRET")
+        });
     }
 }
